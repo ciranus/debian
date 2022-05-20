a13b2dddb8d6c1a51c0153e849e82fc7ef7ee7a8
ciranus <61602511+ciranus@users.noreply.github.com>
2022-05-20 16:21:16 +0200

    Update ct-report.sh

--- ct-report.sh
+++ ct-report.sh
@@ -13,6 +13,6 @@ grep -Phrs '^d.*(debian|ubuntu)' /etc/apt/{,sources.list.d/}*.list
 
 printf '\n\x1b[32m● Cherrytree dependencies:\x1b[0m\n\n'
 
-dpkg -l |awk '/^i.*(cherryt|lib(c6|glib2.0-0|gcc-s1|stdc|fmt|spd|atkm|curl4|gspell-1-.:|gtksourceviewmm|sqlite3|svg2|uchardet0|xml\+))/&&!/-dev/{sub(":a[^ ]*","");printf"%-27s %s\n",$2,$3}'
+dpkg -l |awk '/^i.*(cherryt|lib(c6|glib2.0-0|gcc-s1|stdc|fmt|spd|atkm|curl4|gspell-1-.:|gtksourceviewmm|sqlite3|rsvg2|uchardet0|xml\+))/&&!/-dev/{sub(":a[^ ]*","");printf"%-27s %s\n",$2,$3}'
 
 exit
