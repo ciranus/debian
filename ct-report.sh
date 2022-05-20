60bca597a5b9af06a74ca849242379b1da5c9761
ciranus <61602511+ciranus@users.noreply.github.com>
2022-05-20 16:15:00 +0200

    Add files via upload
    Cherrytree installation report for Debian/Ubuntu

---
+++ ct-report.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+printf '\n\x1b[32m● Distribution identification: \x1b[0m'
+
+  if [[ -f /etc/lsb-release ]]   ; then awk -F= '/DESC/ {print $2}' /etc/lsb-release
+elif [[ -f /etc/debian_version ]]; then cat /etc/debian_version
+else echo 'unknown'
+  fi
+
+printf '\n\x1b[32m● Debian / Ubuntu repositories:\x1b[0m\n\n'
+
+grep -Phrs '^d.*(debian|ubuntu)' /etc/apt/{,sources.list.d/}*.list
+
+printf '\n\x1b[32m● Cherrytree dependencies:\x1b[0m\n\n'
+
+dpkg -l |awk '/^i.*(cherryt|lib(c6|glib2.0-0|gcc-s1|stdc|fmt|spd|atkm|curl4|gspell-1-.:|gtksourceviewmm|sqlite3|svg2|uchardet0|xml\+))/&&!/-dev/{sub(":a[^ ]*","");printf"%-27s %s\n",$2,$3}'
+
+exit
