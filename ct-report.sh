3d615a2c201ff3b3cc0b8f0f91ad286a9a0c9920
ciranus <61602511+ciranus@users.noreply.github.com>
2022-05-20 18:15:16 +0200

    Update ct-report.sh

--- ct-report.sh
+++ ct-report.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-printf '\n\x1b[32m● Distribution identification: \x1b[0m'
-
-  if [[ -f /etc/lsb-release ]]   ; then awk -F= '/DESC/ {print $2}' /etc/lsb-release
-elif [[ -f /etc/debian_version ]]; then cat /etc/debian_version
-else echo 'unknown'
+  if [[ -f /etc/lsb-release ]]   ; then D=$(awk -F= '/DESC/ {print $2}' /etc/lsb-release)
+elif [[ -f /etc/debian_version ]]; then D=$(cat /etc/debian_version)
+else D='unknown'
   fi
 
+printf "\n\x1b[32m● Distribution identification:\x1b[0m $D ~ $(env |awk -F= '/CURRENT_D/ {print $2}')\n"
 printf '\n\x1b[32m● Debian / Ubuntu repositories:\x1b[0m\n\n'
 
 grep -Phrs '^d.*(debian|ubuntu)' /etc/apt/{,sources.list.d/}*.list
