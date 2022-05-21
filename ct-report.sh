c1ccec9f0d940371fcf66333b4ad9760da4d4370
ciranus <61602511+ciranus@users.noreply.github.com>
2022-05-21 10:47:50 +0200

    Update ct-report.sh

--- ct-report.sh
+++ ct-report.sh
@@ -16,7 +16,7 @@ else D='unknown'
 printf "\n\x1b[32m● Distribution identification:\x1b[0m $D ~ $(env |awk -F= '/CURRENT_D/ {print $2}')\n"
 printf '\n\x1b[32m● Debian / Ubuntu repositories:\x1b[0m\n\n'
 
-grep -Phrs '^d.*(debian|ubuntu|cherrytree)' /etc/apt/{,sources.list.d/}*.list
+grep -Phrs '^d.*(debian|ubuntu|giuspen)' /etc/apt/{,sources.list.d/}*.list |grep -v 'security\|updates'
 
 printf '\n\x1b[32m● Cherrytree dependencies:\x1b[0m\n\n'
 
