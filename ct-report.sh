beae30a40d32f6be16948a73f4ccbf7c99988289
ciranus <61602511+ciranus@users.noreply.github.com>
2022-05-24 13:27:27 +0200

    Update ct-report.sh

--- ct-report.sh
+++ ct-report.sh
@@ -11,15 +11,15 @@ printf '\n\x1b[32m● Debian / Ubuntu repositories:\x1b[0m\n\n'
 grep -Phrs '^d.*((debian|ubuntu).*main|giuspen)' /etc/apt/{,sources.list.d/}*.list |grep -v 'security\|updates'
 
 if dpkg -l cherrytree |grep -q '^i' ; then dpkg -l cherrytree | sed -n '/cherry/{s/ii //;s/amd64.*/ : installed/;p}'
-else printf '\ncompatibility check -> '; apt-cache policy libfmt{7,8} |sed -n '1{s/://;p}' ; echo
+else printf '\ncompatibility check -> '; apt-cache policy libfmt{7,8} |sed -n '1{s/://;p}' ;echo
 fi
 
 printf "\n\x1b[32m● Cherrytree dependencies status:\x1b[0m [$(date +%Y-%m-%d)]\n\n"
 
 dpkg -l |awk '$2~/lib(c6|glib2.0-0|gcc-s1|stdc|fmt|spd|atkm|curl4|gspell-1-.:|gtksourceviewmm|sqlite3|rsvg2-c|uchardet0|xml\+)/&&/^i/&&!/-dev/{sub(":a[^ ]*","");printf"%-27s %s\n",$2,$3}'
 echo
-printf 'Document file size/type:\t';ls -shS --format=single-colum $(awk -F = '/doc_0/ {print $2}' ~/.config/cherrytree/config.cfg) | sed 's/\/.*ctb/sqlite/;s/\/.*ctd/xml/'
-echo
+printf '\x1b[33mDocument size/type:\x1b[0m  ';ls -shS --format=single-colum $(awk -F = '/doc_0/ {print $2}' ~/.config/cherrytree/config.cfg) | sed 's/\/.*ctb/sqlite/;s/\/.*ctd/xml/'
+printf '\n\x1b[33mConfig:\x1b[0m\n'
 grep 'backup' ~/.config/cherrytree/config.cfg
 echo
 exit
