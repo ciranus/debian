ada9fce7752eca84e19384a0c9b4509f70b89ec6
ciranus <61602511+ciranus@users.noreply.github.com>
2022-05-24 13:18:40 +0200

    Update ct-report.sh

--- ct-report.sh
+++ ct-report.sh
@@ -5,16 +5,22 @@
 # no need to run as root + not recommended
 [[ $USER = root ]] && printf '\x1b[35mNo need to run this script as root. You can, but not recommended\nBut ok...\x1b[0m\n'
 
-D=$(sed -n '/PRETTY/{s/^.*=//;s/GNU.Linux //;p}' /etc/os-release)
-
-printf "\n\x1b[32m● Distribution Id:\x1b[0m $D ~ $(env |awk -F= '/CURRENT_D/ {print $2}')\n"
+printf "\n\x1b[32m● Distribution Id:\x1b[0m $(sed -n '/PRETTY/{s/^.*=//;s/GNU.Linux //;p}' /etc/os-release) ~ $(env |awk -F= '/CURRENT_D/ {print $2}')\n"
 printf '\n\x1b[32m● Debian / Ubuntu repositories:\x1b[0m\n\n'
 
 grep -Phrs '^d.*((debian|ubuntu).*main|giuspen)' /etc/apt/{,sources.list.d/}*.list |grep -v 'security\|updates'
 
+if dpkg -l cherrytree |grep -q '^i' ; then dpkg -l cherrytree | sed -n '/cherry/{s/ii //;s/amd64.*/ : installed/;p}'
+else printf '\ncompatibility check -> '; apt-cache policy libfmt{7,8} |sed -n '1{s/://;p}' ; echo
+fi
+
 printf "\n\x1b[32m● Cherrytree dependencies status:\x1b[0m [$(date +%Y-%m-%d)]\n\n"
 
-dpkg -l |awk '$2~/cherryt|lib(c6|glib2.0-0|gcc-s1|stdc|fmt|spd|atkm|curl4|gspell-1-.:|gtksourceviewmm|sqlite3|rsvg2-c|uchardet0|xml\+)/&&/^i/&&!/-dev/{sub(":a[^ ]*","");printf"%-27s %s\n",$2,$3}'
+dpkg -l |awk '$2~/lib(c6|glib2.0-0|gcc-s1|stdc|fmt|spd|atkm|curl4|gspell-1-.:|gtksourceviewmm|sqlite3|rsvg2-c|uchardet0|xml\+)/&&/^i/&&!/-dev/{sub(":a[^ ]*","");printf"%-27s %s\n",$2,$3}'
 dpkg -l cherrytree |grep -q '^i' || (printf '\nlibfmt compatibility check -> '; apt-cache policy libfmt{7,8} |sed -n '1{s/://;p}')
-
+echo
+printf 'Document file size/type:\t';ls -shS --format=single-colum $(awk -F = '/doc_0/ {print $2}' ~/.config/cherrytree/config.cfg) | sed 's/\/.*ctb/sqlite/;s/\/.*ctd/xml/'
+echo
+grep 'backup' ~/.config/cherrytree/config.cfg
+echo
 exit
